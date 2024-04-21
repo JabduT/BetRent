2993d0af423e1e6b8a8ef270e1c23530a2859fb3
tokukiyu <tokkiyuam@gmail.com>
2024-04-21 17:48:34 +0300

    renter app

--- owner_app/lib/main.dart
+++ owner_app/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       initialRoute: '/',
       routes: {
