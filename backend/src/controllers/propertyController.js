3d1cbd5611381eda1587be78db26bb9f5978c607
tokukiyu <tokkiyuam@gmail.com>
2024-04-21 12:29:47 +0300

    list, search and filter initial server side

--- backend/src/controllers/propertyController.js
+++ backend/src/controllers/propertyController.js
@@ -3,10 +3,77 @@
 const { Property, validateProperty } = require("../models/property");
 const Joi = require("joi");
 
+//Get All product
+class APIfeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+  multfilter() {
+    const searchQuery = this.queryString.q || "";
+    if (typeof searchQuery === "string") {
+      const regexSearch = {
+        $or: [
+          { title: { $regex: searchQuery, $options: "i" } },
+          { categories: { $regex: searchQuery, $options: "i" } },
+        ],
+      };
+      this.query.find(regexSearch);
+    }
+    return this;
+  }
+  filter() {
+    //1 build query
+    const queryObj = { ...this.queryString };
+    const excludedFields = ["page", "limit", "sort", "fields", "q"];
+    excludedFields.forEach((el) => delete queryObj[el]);
+    // advanced query
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(
+      /\b(gte|gt|lte|lt|eq)\b/g,
+      (match) => `$${match}`
+    );
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" ");
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("-createdAt");
+    }
+    return this;
+  }
+  limiting() {
+    if (this.queryString.fields) {
+      const selectedFields = this.queryString.fields.split(",").join(" ");
+      this.query = this.query.select(selectedFields);
+    } else {
+      this.query = this.query.select("-__v");
+    }
+    return this;
+  }
+  paginatinating() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 10;
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
+}
+
 // Get all properties
 exports.getAllProperties = async (req, res) => {
   try {
-    const properties = await Property.find();
+    const features = new APIfeatures(Property.find(), req.query)
+      .multfilter()
+      .filter()
+      .sort()
+      .limiting()
+      .paginatinating();
+    const properties = await features.query.select();
+    // const properties = await Property.find();
     res.json(properties);
   } catch (error) {
     res.status(500).json({ message: error.message });
--- backend/src/models/property.js
+++ backend/src/models/property.js
@@ -1,53 +1,102 @@
-// models/Property.js
+const mongoose = require("mongoose");
+const Joi = require("joi");
 
-const mongoose = require('mongoose');
-const Joi = require('joi');
+const propertySchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 255,
+    },
+    type: {
+      type: String,
+      enum: [
+        "Apartment",
+        "Condo",
+        "House",
+        "Studio Apartment",
+        "Villa",
+        "Bedsitter",
+        "Block of Flats",
+        "Chalet",
+        "Duplex",
+        "Farm House",
+        "Mansion",
+        "Penthouse",
+        "Room & Parlour",
+        "Shared Apartment",
+        "Townhouse / Terrace",
+      ],
+      default: "House",
+    },
+    description: {
+      type: String,
+      minlength: 10,
+      maxlength: 2000,
+    },
+    roomNumber: { type: Number },
+    bedRoomNum: { type: Number },
+    address: {
+      type: String,
+      required: true,
+      minlength: 10,
+      maxlength: 1000,
+    },
+    files: {
+      type: [String],
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  },
+  { timestamps: true }
+);
 
-const propertySchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 255
-  },
-  description: {
-    type: String,
-    required: true,
-    minlength: 10,
-    maxlength: 2000
-  },
-  address: {
-    type: String,
-    required: true,
-    minlength: 10,
-    maxlength: 1000
-  },
-  files: {
-    type: [String],
-    required: true
-  },
-  price: {
-    type: Number,
-    required: true,
-    min: 0
-  },
-  // Additional attributes can be added as needed
-});
-
-const Property = mongoose.model('Property', propertySchema);
+const Property = mongoose.model("Property", propertySchema);
 
 function validateProperty(property) {
   const schema = Joi.object({
     title: Joi.string().min(5).max(255).required(),
+    type: Joi.string()
+      .valid(
+        "Apartment",
+        "Condo",
+        "House",
+        "Studio Apartment",
+        "Villa",
+        "Bedsitter",
+        "Block of Flats",
+        "Chalet",
+        "Duplex",
+        "Farm House",
+        "Mansion",
+        "Penthouse",
+        "Room & Parlour",
+        "Shared Apartment",
+        "Townhouse / Terrace"
+      )
+      .default("House"),
     description: Joi.string().min(10).max(2000).required(),
+    roomNumber: Joi.number().integer().min(0).required(),
+    bedRoomNum: Joi.number().integer().min(0).required(),
     address: Joi.string().min(10).max(1000).required(),
+    userId: Joi.string().required(),
     files: Joi.array().items(Joi.string()).required(),
-    price: Joi.number().min(0).required()
+    price: Joi.number().min(0).required(),
   });
   return schema.validate(property);
 }
 
 module.exports = {
   Property,
-  validateProperty
+  validateProperty,
 };
