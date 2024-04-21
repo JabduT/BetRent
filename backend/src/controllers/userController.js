4da209a8a90dcc6272639245e07d934be48b3b65
tokukiyu <tokkiyuam@gmail.com>
2024-04-21 10:21:36 +0300

    some

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -7,7 +7,7 @@ const Joi = require('joi');
 exports.getAllUsers = async (req, res) => {
   try {
     const users = await User.find();
-    res.json(users);
+    res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -24,6 +24,7 @@ exports.createUser = async (req, res) => {
   try {
     const user = new User(req.body);
     await user.save();
+    
     res.status(201).json(user);
   } catch (error) {
     res.status(500).json({ message: error.message });
