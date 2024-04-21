5c68b73b97a9db065905d1b33f34731dcd219b26
tokukiyu <tokkiyuam@gmail.com>
2024-04-21 10:48:18 +0300

    user model update

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,41 +1,58 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
 
-const userSchema = new mongoose.Schema({
-  //   username: {
-  //     type: String,
-  //     required: true,
-  //     minlength: 3,
-  //     maxlength: 50
-  //   },
-  email: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 255,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    //   username: {
+    //     type: String,
+    //     required: true,
+    //     minlength: 3,
+    //     maxlength: 50
+    //   },
+
+    phoneNumber: {
+      type: String,
+      required: true,
+      minlength: 10,
+      maxlength: 15,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return /^\+[1-9]\d{1,14}$/.test(v);
+        },
+        message: (props) =>
+          `${props.value} is not a valid phone number with prefix!`,
+      },
+    },
+    PIN: {
+      type: Number,
+      required: true,
+      min: 1000,
+      max: 999999,
+    },
+
+    role: {
+      type: String,
+      enum: ["renter", "landlord"],
+      default: "renter",
+    },
   },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-    maxlength: 1024,
-  },
-  role: {
-    type: String,
-    enum: ["renter", "landlord"],
-    default: "renter",
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
 function validateUser(user) {
   const schema = Joi.object({
     // username: Joi.string().min(3).max(50).required(),
-    email: Joi.string().min(5).max(255).required().email(),
-    password: Joi.string().min(8).max(255).required(),
+    email: Joi.string().min(5).max(255).email(),
+    PIN: Joi.number().integer().required().min(1000).max(999999),
     role: Joi.string().valid("renter", "landlord").default("renter"),
+    phoneNumber: Joi.string()
+      .min(10)
+      .max(15)
+      .required()
+      .regex(/^\+[1-9]\d{1,14}$/),
   });
   return schema.validate(user);
 }
