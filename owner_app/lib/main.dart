52e4a1ed065292b07ae767f49ff74955f121f56a
Lalisa <bulalalisa@gmail.com>
2024-04-21 22:05:08 -0500

    removed

--- owner_app/lib/main.dart
+++ owner_app/lib/main.dart
@@ -12,10 +12,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
+@override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
