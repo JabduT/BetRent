c0490e55f2ea57c8b6aa85057b80814b5d08ecfa
Lalisa <bulalalisa@gmail.com>
2024-04-24 17:49:06 -0500

    test

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -29,9 +29,10 @@ const router = express.Router();
 router.post("/login", Login);
 router.post('/users/create',createUser);
 //view
+router.get("users/profile",authenticate,getMe,getUserById);
 router.get("/users", getAllUsers);
-router.get("/users/:id", getUserById);
-router.route("users/profile").get(authenticate,getMe,getUserById).put(getMe,updateUser);
+//router.get("/users/:id", getUserById);
+//put(getMe,updateUser);
 router.put("/users/:id", updateUser);
 router.delete("/users/:id", deleteUser);
 
