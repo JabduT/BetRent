8027c565e9d136bd43e16b7a169097ff933640eb
Tokuma Abdisa <tokkiyuam@gmail.com>
2024-04-25 12:24:49 +0300

    main update

--- mobileApp/lib/main.dart
+++ mobileApp/lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:owner_app/screens/authentication/register.dart';
 import 'package:owner_app/screens/chatting_screen.dart';
 import 'package:owner_app/screens/renter/property_list_screen.dart';
@@ -17,23 +19,69 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      initialRoute: '/owner_home',// pls do this part 
-      routes: {
-        '/login': (context) => LoginScreen(),
-        '/register': (context) => SignUpScreen(),
-        '/owner_home': (context) => OwnerBottomNavigationBar(),
-        '/renter_home': (context) => RenterBottomNavigationBar(),
-        '/screen1': (context) => PropertyListScreen(),
-        // '/chat': (context) => ChattingScreen(), // Use the new ChattingScreen
-
-        '/screen4': (context) => Screen4(),
+    return FutureBuilder(
+      // Fetch user data asynchronously
+      future: getUserData(),
+      builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator(); // Loading indicator
+        } else {
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            String? role = snapshot.data;
+            // Role-based navigation
+            if (role == 'renter') {
+              return MaterialApp(
+                debugShowCheckedModeBanner: false,
+                title: 'Flutter Demo',
+                theme: ThemeData(
+                  primarySwatch: Colors.red,
+                ),
+                initialRoute: '/renter_home',
+                routes: {
+                  '/login': (context) => LoginScreen(),
+                  '/register': (context) => SignUpScreen(),
+                  '/renter_home': (context) => RenterBottomNavigationBar(),
+                  '/screen1': (context) => PropertyListScreen(),
+                  // '/chat': (context) => ChattingScreen(),
+                  '/screen4': (context) => Screen4(),
+                },
+              );
+            } else {
+              return MaterialApp(
+                debugShowCheckedModeBanner: false,
+                title: 'Flutter Demo',
+                theme: ThemeData(
+                  primarySwatch: Colors.red,
+                ),
+                initialRoute: '/owner_home',
+                routes: {
+                  '/login': (context) => LoginScreen(),
+                  '/register': (context) => SignUpScreen(),
+                  '/owner_home': (context) => OwnerBottomNavigationBar(),
+                  '/screen1': (context) => PropertyListScreen(),
+                  // '/chat': (context) => ChattingScreen(),
+                  '/screen4': (context) => Screen4(),
+                },
+              );
+            }
+          }
+        }
       },
     );
+  }
+
+  // Function to get user data asynchronously
+  Future<String?> getUserData() async {
+    final storage = FlutterSecureStorage();
+    final String? userData = await storage.read(key: 'user');
+
+    if (userData != null) {
+      final user = jsonDecode(userData);
+      return user['role'];
+    } else {
+      return null;
+    }
   }
 }
