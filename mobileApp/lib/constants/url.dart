a03580203b1e04996c87ec6f11103909e4ecefe4
Tokuma Abdisa <tokkiyuam@gmail.com>
2024-04-26 11:44:47 +0300

    update

--- mobileApp/lib/constants/url.dart
+++ mobileApp/lib/constants/url.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppConstants {
   static final String tokURL = "http://192.168.88.20:5000/api";
-  static final String APIURL = "http://127.0.0.1:5000/api";
+  static final String APIURL = "http://192.168.88.20:5000/api";
   static final String BASEURL = "http://192.168.88.20:5000";
   static final Color secondaryColor = Color.fromRGBO(248, 189, 13, 1.0);
 
